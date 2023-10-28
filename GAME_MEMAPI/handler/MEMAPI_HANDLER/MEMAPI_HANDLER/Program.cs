de530894e38f33e659a7ddeab22bec40b31c54c3
Валодя Петров <96941376+Ma-ZaHaKa@users.noreply.github.com>
2023-10-28 20:06:52 +0300

    Add files via upload

---
+++ GAME_MEMAPI/handler/MEMAPI_HANDLER/MEMAPI_HANDLER/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MEMAPI_HANDLER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
