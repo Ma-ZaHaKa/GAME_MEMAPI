cbbaaf4d2d2c0c3aa8c661e33f2b68734106b11a
Валодя Петров <96941376+Ma-ZaHaKa@users.noreply.github.com>
2023-10-30 01:15:31 +0300

    Add files via upload

--- GAME_MEMAPI/arduino/VehicleDevices/VehicleDevices.ino
+++ GAME_MEMAPI/arduino/VehicleDevices/VehicleDevices.ino
@@ -1,13 +1,95 @@
+#include <ArduinoJson.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <LCD_1602_RUS.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // Устанавливаем дисплей
+LCD_1602_RUS lcd_ru(0x27,16,2); // присваиваем имя LCD для дисплея
+
+const int JSON_BUFFER_SIZE = 256;
+String PROJ_CODE = "veh_device";
+
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  lcd.init();                     
+  lcd.backlight();
+  
+  lcd_ru.init();
+  lcd_ru.backlight();
+}
+
+/*void PrintLCD(const char* text, int row = 0, int col = 0)
+{
+  lcd.clear();
+  lcd.setCursor(col, row);
+  lcd.print(text);
+}
+
+void PrintLCD_RU(const char* text, int row = 0, int col = 0)
+{
+  lcd_ru.clear();
+  lcd_ru.setCursor(col, row);
+  lcd_ru.print(text);
+}*/
+
+void PrintLCD(String text, int row = 0, int col = 0)
+{
+  lcd.clear();
+  lcd.setCursor(col, row);
+  lcd.print(text);
+}
+
+void PrintLCD_RU(String text, int row = 0, int col = 0)
+{
+  lcd_ru.clear();
+  lcd_ru.setCursor(col, row);
+  lcd_ru.print(text);
 }
 
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  if (Serial.available())
+  {
+    String inputString = Serial.readStringUntil('\n');
+
+    // Попытка разобрать JSON
+    DynamicJsonDocument jsonDoc(JSON_BUFFER_SIZE);
+    DeserializationError error = deserializeJson(jsonDoc, inputString);
+
+    if (!error)
+    {
+      if (jsonDoc.containsKey("mode"))
+      {
+        if(jsonDoc["mode"] == "print" && jsonDoc.containsKey("value"))
+        {
+          String value = jsonDoc["value"].as<String>();
+          PrintLCD_RU(value);
+        }
+        
+        else if(jsonDoc["mode"] == "print_wstr" && jsonDoc.containsKey("value") && jsonDoc.containsKey("string"))
+        { // {"mode":"print_wstr","value":"123","string":"пвапав","row":"1"}
+          String string = jsonDoc["string"].as<String>();
+          String value = jsonDoc["value"].as<String>();
+          int row = 0;
+          if(jsonDoc.containsKey("row")) { row = jsonDoc["row"].as<String>().toInt(); if((row < 0) || (row > 1)){ row = 0; } }
+          /*int a = 42;
+          float b = 3.14;
+          String result = String(a) + ": " + String(b);*/
+          String out = string + ": " + value;
+          PrintLCD_RU(out, row);
+        }
+              
+        else if (jsonDoc["mode"] == "hello")
+        {
+          DynamicJsonDocument responseDoc(JSON_BUFFER_SIZE);
+          responseDoc["data"] = PROJ_CODE;
+          serializeJson(responseDoc, Serial);
+          Serial.println();
+        }
+                
+      }
+    }
+  }
 }
