a3fb4450afca418407ad7e02c4c4315f18239280
Валодя Петров <96941376+Ma-ZaHaKa@users.noreply.github.com>
2023-10-29 03:50:16 +0300

    Add files via upload

---
+++ GAME_MEMAPI/handler/MEMAPI_HANDLER/MEMAPI_HANDLER/Methods/GMA_API.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.IO.Ports;
+using System.Reflection;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace MEMAPI_HANDLER
+{
+    partial class Program
+    {
+        static string GetPointerJSON(string module, int base_offset, List<int> offsets)
+        {
+            var jsonObject = new
+            {
+                mode = "get_pointer",
+                module = $"{module}",
+                base_offset = $"{base_offset}",
+                offsets = offsets.ToArray(),
+                //offsets = new[] { "0x2C0" },
+            };
+
+            string json = JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
+            return json;
+        }
+        static string GetPointerJSON(string module, string base_offset, List<string> offsets)
+        {
+            var jsonObject = new
+            {
+                mode = "get_pointer",
+                module = $"{module}",
+                base_offset = $"{base_offset}",
+                offsets = offsets.ToArray(),
+                //offsets = new[] { "0x2C0" },
+            };
+
+            string json = JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
+            return json;
+        }
+    }
+}
--- GAME_MEMAPI/handler/MEMAPI_HANDLER/MEMAPI_HANDLER/Program.cs
+++ GAME_MEMAPI/handler/MEMAPI_HANDLER/MEMAPI_HANDLER/Program.cs
@@ -1,51 +1,47 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.IO.Ports;
-
+using System.Reflection;
+using System.Threading.Tasks;
+using WebSocketSharp;
 
 namespace MEMAPI_HANDLER
 {
-    class Program
+    partial class Program
     {
+        //public static string last_msg = "";
+        //static void OnMsg_(object sender, MessageEventArgs e)
+        //{
+        //    //if(e.Data == "Hello!")
+        //    //Console.WriteLine($"Received message: {e.Data}");
+        //    //socket.Send(text);
+        //    last_msg = e.Data;
+        //}
+
+
+
+
+
+
+        static string GMA_START_MSG = "HI";
+        static string GMA_DEV_RESPONSE_MSG = "HI_GMA";
+
         static void Main(string[] args)
         {
-            string messageToSend = "Hello";
-            string expectedResponse = "hi";
-            string foundPort = null;
+            //string foundPort = FindCOMPort(GMA_START_MSG, GMA_DEV_RESPONSE_MSG);
+            //if (foundPort == "") { Console.WriteLine($"No port with '{GMA_DEV_RESPONSE_MSG}' response found."); return; }
 
-            string[] portNames = SerialPort.GetPortNames();
+            int GMA_PORT = 9005;
+            bool status_connect = UWSConect(GMA_PORT);
+            if (!status_connect) { Console.WriteLine($"WS CANT CONNECT TO PORT {GMA_PORT}"); return; }
+            //string json = GetPointerJSON("gta3.exe", "0x5412F0", new List<string> { "0x2C0" });
+            string json = GetPointerJSON("gta3.exe", "0x5412F0", new List<string> { "0x2C0" });
+            socket.Send(json);
+            Console.WriteLine($"{GetMessage()}");
 
-            foreach (string portName in portNames)
-            {
-                using (SerialPort port = new SerialPort(portName))
-                {
-                    try
-                    {
-                        port.Open();
-                        port.Write(messageToSend);
-                        System.Threading.Thread.Sleep(100); // Дайте время на обработку и ответ
-
-                        string response = port.ReadExisting();
-                        if (response.Contains(expectedResponse))
-                        {
-                            foundPort = portName;
-                            break; // Выход из цикла, если найден нужный порт
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Error communicating with {portName}: {ex.Message}");
-                    }
-                }
-            }
-
-            if (!string.IsNullOrEmpty(foundPort))
-            {
-                Console.WriteLine($"Found the 'hi' response on port: {foundPort}");
-            }
-            else
-            {
-                Console.WriteLine("No port with 'hi' response found.");
-            }
         }
     }
 }
