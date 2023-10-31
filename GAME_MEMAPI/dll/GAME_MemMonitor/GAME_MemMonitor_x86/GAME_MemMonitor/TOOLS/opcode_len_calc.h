dfe809ed8ebabefac2cc67fb639bebe5aab6069b
Валодя Петров <96941376+Ma-ZaHaKa@users.noreply.github.com>
2023-10-31 06:05:05 +0300

    Delete GAME_MEMAPI/dll/GAME_MemMonitor directory

--- GAME_MEMAPI/dll/GAME_MemMonitor/GAME_MemMonitor_x86/GAME_MemMonitor/TOOLS/opcode_len_calc.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-unsigned int InstructionLength(unsigned char *pc);
-
-template <typename T> unsigned int InstructionLength(T *pc)
-{
-	return InstructionLength((unsigned char *)pc);
-}
